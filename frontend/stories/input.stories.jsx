ca77ac5a9bca3f615e03beb5eb78b2dc4362c775
Ryan Fong <ryan.fong@sbcglobal.net>
2021-08-23 22:49:04 -0700

    Add pagination, fix footer css

--- frontend/stories/input.stories.jsx
+++ frontend/stories/input.stories.jsx
@@ -2,14 +2,21 @@ import React from 'react';
 import Input from '../components/forms/Input/input';
 
 export default {
-  title: 'Components/Input',
+  title: 'Components/Forms/Input',
   component: Input,
+  subcomponents: { InputWithIcon, FullInput },
   args: {
-      labelName: "Label",
-      onChange: () => {}
-  }
+    labelName: 'Label',
+    subLabel: <span className="optional ml-1">(Sublabel)</span>,
+    subtitle: 'subtitle',
+    inputClassName: 'channel-name-input',
+    icon: <div className="input-icon-storybook">★</div>,
+    onChange: () => {},
+  },
 };
 
-const Template = (args) => <Input {...args} />;
+export const Default = (args) => <Input labelName={args.labelName} onChange={args.onChange} />;
 
-export const InputStory = Template.bind({});
+export const InputWithIcon = args => <Input labelName={args.labelName} onChange={args.onChange} icon={args.icon} inputClassName={args.inputClassName} />
+
+export const FullInput = args => <Input {...args}  />
